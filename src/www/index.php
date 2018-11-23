eef12c670d6380e389b1a03267b7c0e8f8439a96
David Hrdlička <dhrdlicka@users.noreply.github.com>
2018-11-23 19:07:13 +0100

    Web stuff

---
+++ src/www/index.php
@@ -0,0 +1,31 @@
+<h1>Summary</h1>
+<table>
+<tr>
+<th>IP address</th>
+<th>Hostname</th>
+</tr>
+<?php
+$txt_file    = file_get_contents('/etc/hosts');
+$rows        = explode("\n", $txt_file);
+array_shift($rows);
+
+foreach($rows as $row => $data)
+{
+    if($data[0] !== '#')
+    {
+    echo '<tr>';
+    //get row data
+    $row_data = explode(' ', str_replace("	"," ",$data), 2);
+
+    $info[$row]['ip'] = $row_data[0];
+    $info[$row]['hostname'] = $row_data[1];
+
+    //display data
+    echo '<td>' . $info[$row]['ip'] . '</td>';
+    echo '<td>' . $info[$row]['hostname'] . '</td>';
+
+    echo '</tr>';
+    }
+}
+?>
+</table>
