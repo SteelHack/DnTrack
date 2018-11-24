ec4af097c6941339c02747bd39e4757740dadf73
David Hrdlička <dhrdlicka@users.noreply.github.com>
2018-11-24 01:44:17 +0100

    Update configurator

--- src/dnsPython/configurator.py
+++ src/dnsPython/configurator.py
@@ -5,15 +5,26 @@ import time
 from stat import * # ST_SIZE etc
 import shutil
 
-prevChan = 0;
+prevuChan = 0;
+prevaChan = 0;
 
 while True:
 	#print "DNSmasq conf checker script initiated...";
-	currChan = os.stat("/var/www/html/dnsmasq.conf")[ST_MTIME];
-	if currChan != prevChan:
-		print "DEBUG: dnsmasq config file changed!";
+	curruChan = os.stat("/var/www/html/conf/userDNS.txt")[ST_MTIME];
+	curraChan = os.stat("/var/www/html/conf/autoDNS.txt")[ST_MTIME];
+	if curruChan != prevuChan or curraChan != prevaChan:
+		print "Some configuration changed!";
+		#next line won't be used hopefully... Delete next time.
 		#copyfile('/var/www/html/dnsmasq.conf', '/etc/dnsmasq.conf');
-		prevChan = currChan;
-	else:
-		print "..";
+		userDNSs = open("/var/www/html/conf/userDNS.txt", 'r');
+		autoDNSs = open("/var/www/html/conf/autoDNS.txt", 'r');
+		hostsConf = open("/var/www/html/conf/dnsmasq.hosts", 'w');
+		udns = userDNSs.read();
+		adns = autoDNSs.read();
+		hostsConf.write(udns + "\n\n" + adns);
+		userDNSs.close();
+		autoDNSs.close();
+		hostsConf.close();
+		prevuChan = curruChan;
+		prevaChan = curraChan;
 	time.sleep(1);
