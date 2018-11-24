52500cf862bf3f8827d9f76c271d75c9b1c6bc43
David Hrdlička <dhrdlicka@users.noreply.github.com>
2018-11-24 02:08:55 +0100

    Update parser

--- src/dnsPython/parse.py
+++ src/dnsPython/parse.py
@@ -2,18 +2,17 @@
 
 import sys
 import re
+import urllib
 
 file = open("autoDNS.txt", "a")
-while True:    
-    try:        
-        line = input()
-        if not line.startswith("!"): # ignore comments
-            match = False
-            m = re.search("([A-Za-z0-9]+?([A-Za-z0-9_\-]*?[A-Za-z0-9]+?)*\.)+?[A-Za-z0-9]{2,}", line) # is there a domain name?
+filter = urllib.request.urlopen("https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt") # it's a file like object and works just like a file
+    
+for line in filter:
+    line = line.decode("utf-8")
+    if not line.startswith("!"): # ignore comments
+        match = False
+        m = re.search("([A-Za-z0-9]+?([A-Za-z0-9_\-]*?[A-Za-z0-9]+?)*\.)+?[A-Za-z0-9]{2,}", line) # is there a domain name?
+        if(isinstance(m, re.Match)):
+            m = re.match("^\|\|(.+?)\^(\$[a-zA-Z0-9\-]+?)?$", line)
             if(isinstance(m, re.Match)):
-                m = re.match("^\|\|(.+?)\^(\$[a-zA-Z0-9\-]+?)?$", line)
-                if(isinstance(m, re.Match)):
-                    file.write("127.0.0.1 " + m.group(1) + "\n")
-    except EOFError:
-        file.close()
-        break
+                file.write("127.0.0.1 " + m.group(1) + "\n")
