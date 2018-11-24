4b8ad6a64829bf1cf6b391cd9d921e1e89cba237
David Hrdlička <dhrdlicka@users.noreply.github.com>
2018-11-24 02:52:48 +0100

    Fix parser

--- src/dnsPython/parse.py
+++ src/dnsPython/parse.py
@@ -1,18 +1,20 @@
+#!/usr/bin/python3
 # Parses AdBlock/uBlockOrigin filters into /etc/hosts entries  
 
 import sys
 import re
-import urllib
+import urllib.request
 
 file = open("autoDNS.txt", "a")
-filter = urllib.request.urlopen("https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt") # it's a file like object and works just like a file
+filter = urllib.request.urlopen("https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt")
     
 for line in filter:
     line = line.decode("utf-8")
     if not line.startswith("!"): # ignore comments
         match = False
-        m = re.search("([A-Za-z0-9]+?([A-Za-z0-9_\-]*?[A-Za-z0-9]+?)*\.)+?[A-Za-z0-9]{2,}", line) # is there a domain name?
-        if(isinstance(m, re.Match)):
+        m = re.search("([A-Za-z0-9]+?([A-Za-z0-9_\-]*?[A-Za-z0-9]+?)*\.)+?[A-Za-z0-9]{2,}", line) # is there a domain name?¨
+        if not m == None:
             m = re.match("^\|\|(.+?)\^(\$[a-zA-Z0-9\-]+?)?$", line)
-            if(isinstance(m, re.Match)):
+            if not m == None:
                 file.write("127.0.0.1 " + m.group(1) + "\n")
+
