ab46dee4751b9492c8f2aa2f359fc59f91cc68dc
Bohoaush <sin@boprograms.eu>
2018-11-28 09:20:46 +0100

    modified styles of conf webpage

--- src/www/conf/addedUserDNS.php
+++ src/www/conf/addedUserDNS.php
@@ -6,15 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DnTrack configuration</title>
 </head>
-<body>
+<body style="background-color: #000000; color: #FFFFFF; text-align: center;">
     <h1>Add a custom DNS entry</h1>
     <?php
-    if(file_put_contents("/var/www/html/conf/userDNS.txt", $_POST["ip"] . " " . $_POST["hostname"] . "\n", FILE_APPEND) == FALSE)
-        echo "<p>This was not successful</p>";
+    if(file_put_contents("userDNS.txt", $_POST["ip"] . " " . $_POST["hostname"] . "\n", FILE_APPEND) == FALSE)
+        echo "<div style='background-color: #AA3333; color: #440000; width: auto; border-radius: 1em; padding: 30px;'><b>This was not successful!</b></div>";
     else
-        echo "<p>This was successful</p>";
+        echo "<p style='background-color: #33AA33; color: #004400; width: auto; border-radius: 1em; padding: 30px;'><b>Succesfully added custom DNS entry</b></p>";
 
-    echo $_POST["ip"];
+    echo '<br><br>' . $_POST["ip"];
     echo '<br/>';
     echo $_POST["hostname"];
     
